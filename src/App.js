23725748071fafe069752d5f81d98415d3806528
Reese4522 <reeserepositorystudy@gmail.com>
2020-04-22 08:50:27 -0300

    install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto mx-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Row className='mt-4'>
+          <Col>
+            <h1 className='text-primary'>Olá</h1>
+          </Col>
+          <Col>
+            <h1 className='text-danger'>Olá</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
